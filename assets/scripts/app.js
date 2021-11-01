f356d1403170c29fce40f0901838ffe00c03b583
Ayodele abiola Augustine <ayodelaustinab@gmail.com>
2021-11-01 23:53:55 +0100

    first merge

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,26 @@
+<<<<<<< HEAD
 const movieBtn = document.querySelector('header').lastElementChild;
 const addMovie = document.getElementById('add-modal')
 movieBtn.addEventListener('click', () => {
   addMovie.classList.toggle('visible');
 });
+=======
+const startAddMovieBtn = document.querySelector('header button');
+const addMovieBtn = document.getElementById('add-modal');
+const backDrop = document.getElementById('backdrop');
+const btnCancel = document.querySelector('.btn--passive');
+
+const backDropFunction = () => {
+	backDrop.classList.toggle('visible');
+};
+
+startAddMovieBtn.addEventListener('click', () => {
+	addMovieBtn.classList.toggle('visible');
+	backDropFunction();
+});
+
+btnCancel.addEventListener('click', () => {
+	backdrop.classList.toggle('visible');
+	addMovieBtn.classList.toggle('visible');
+});
+>>>>>>> 956201adb82d7c9bc063ee4c872bafe211738856
