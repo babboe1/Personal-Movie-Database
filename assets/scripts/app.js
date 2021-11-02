2e61e67f6282f4f38287fced5cbabbcd711740d1
Ayodele abiola Augustine <ayodelaustinab@gmail.com>
2021-11-02 10:24:06 +0100

    chore:

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,16 +3,16 @@ const addMovieBtn = document.getElementById('add-modal');
 const backDrop = document.getElementById('backdrop');
 const btnCancel = document.querySelector('.btn--passive');
 
-const backDropFunction = () => {
-	backDrop.classList.toggle('visible');
+const backDropToggle = () => {
+	backdrop.classList.toggle('visible');
 };
 
 startAddMovieBtn.addEventListener('click', () => {
 	addMovieBtn.classList.toggle('visible');
-	backDropFunction();
+	backDropToggle();
 });
 
 btnCancel.addEventListener('click', () => {
-	backdrop.classList.toggle('visible');
 	addMovieBtn.classList.toggle('visible');
+	backDropToggle();
 });
